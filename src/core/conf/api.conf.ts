e88add1d7574b640413d3398e99dbc7f752155d4
enbanana <nikita.mazilin@mail.ru>
2026-06-14 20:36:05 +0500

    changed base url

--- src/core/conf/api.conf.ts
+++ src/core/conf/api.conf.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    BASE_URL: import.meta.env.VITE_USER_API_URL || "http:localhost:5173"
+    BASE_URL: import.meta.env.VITE_API_URL || "http:localhost:5173"
 } as const;
