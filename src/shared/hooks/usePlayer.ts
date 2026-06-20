53dff9304e07313396243aa4fcedb0b59577afdd
enbanana <nikita.mazilin@mail.ru>
2026-06-20 18:59:08 +0500

    feat(hooks): new function syncWaller in use player hook

--- src/shared/hooks/usePlayer.ts
+++ src/shared/hooks/usePlayer.ts
@@ -1,14 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "@context";
+
 import { fetcherMe } from "@shared/api";
+import { Player } from "@shared/types";
+import { CurrencyType } from "@/core/conf";
+import Big from "big.js";
 
 export const usePlayer = () => {
+    const queryClient = useQueryClient();
     const { token } = useAuth();
 
-    return useQuery({
+    const syncWallet = (type: CurrencyType, amount: string | number | Big) => {
+        queryClient.setQueryData<Player>(["player"], (oldData) => {
+            if (!oldData) return oldData;
+            return { ...oldData, wallet: { ...oldData.wallet, [type]: new Big(amount) } }
+        });
+    };
+
+    const query = useQuery({
         queryKey: ["player"],
         queryFn: fetcherMe,
         retry: false,
         enabled: !!token
-    })
+    });
+
+    return { ...query, syncWallet }
 }
