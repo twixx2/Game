c3890394ae2d4f7f2810d17e3f384d2db77e305b
enbanana <nikita.mazilin@mail.ru>
2026-07-01 11:51:42 +0500

    added basic ssl, deleted visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    visualizer({
-      open: true,
-      filename: 'stats.html',
-      gzipSize: true,
-    }),
+    basicSsl()
   ],
   css: {
     preprocessorOptions: {
