0dc79cabbc86b4e571cb2437edf20e34289d9038
enbanana <nikita.mazilin@mail.ru>
2026-07-10 15:24:41 +0500

    feat: new loader

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,15 +1,5 @@
 import './loader.css';
 
 export const Loader = () => (
-    <div className="banter-loader">
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-        <div className="banter-loader__box"></div>
-    </div>
+    <p className="loader"><span>chncd</span></p>
 );
